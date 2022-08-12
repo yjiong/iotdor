18c7bbf420638676407495033f4a0a727316b737
yjiong@msn.com <yjiong@msn.com>
2022-08-12 11:02:22 +0800

    fix mariadb dockerfile for new version

--- docker/mysqlup.sh
+++ docker/mysqlup.sh
@@ -28,27 +28,47 @@ if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
 
 # UPDATE user set plugin='mysql_native_password' where user='root' and host='localhost';
 # UPDATE user set authentication_string=password("$MYSQL_ROOT_PASSWORD"),plugin='mysql_native_password' where user='root';
-  cat << EOF > $tfile
-USE mysql;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-UPDATE user set plugin='mysql_native_password' where user='root' and host='localhost';
-FLUSH PRIVILEGES;
+# old version
+#FLUSH PRIVILEGES;
+#USE mysql;
+#GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+#GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+#ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+#UPDATE user set plugin='mysql_native_password' where user='root' and host='localhost';
+#FLUSH PRIVILEGES;
 
+#FLUSH PRIVILEGES;
+#USE mysql;
+#CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'yaojiong'; 
+#ALTER USER 'root'@'localhost' IDENTIFIED BY 'yaojiong';
+#ALTER USER 'root'@'%' IDENTIFIED BY 'yaojiong';
+#GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+#GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+#FLUSH PRIVILEGES;
+  cat << EOF > $tfile
+FLUSH PRIVILEGES;
+USE mysql;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+ALTER USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+ALTER USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
 EOF
 
   if [ "$MYSQL_DATABASE" != "" ]; then
-    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
     echo "[i] Creating database: $MYSQL_DATABASE"
     echo "USE $MYSQL_DATABASE;" >> $tfile
     if [ "$MYSQL_USER" != "" ]; then
       echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-      echo "GRANT ALL ON '$MYSQL_DATABASE'.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+      echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;" >> $tfile
     fi
   fi
+
   /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
   rm -f $tfile
+
 fi
 if [ ! -d /sql ];then
     exec /usr/bin/mysqld --user=root --console
